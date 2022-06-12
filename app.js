b6176c93b528051db368f82d6fc5107952a45837
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-06-12 18:59:50 +0200

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
-app.listen(3000, () => {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
   console.log('Hura!');
 });
