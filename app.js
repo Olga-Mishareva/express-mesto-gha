e0997ecefb441c9d7181dc97731f7ff68e112e40
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-06-13 13:57:04 +0200

    fix: params by getUser & create test user with postman

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const users = require('./routes/users');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/users', users);
-// app.use('/users', users);
-// app.use('/users', users);
 
 app.listen(PORT, () => {
   console.log('Hura!');
