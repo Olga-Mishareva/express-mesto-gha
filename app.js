f9a5abd848df58f13751f7610631e3152fdf4a96
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-06-13 10:40:42 +0200

    feat: add user's model

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
   console.log('Hura!');
