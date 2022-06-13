076391e560de6246a5b6b5d6a1bf5e51bb19ff2e
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-06-13 20:25:16 +0200

    feat: add remove card

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,4 @@
 const Card = require('../models/card');
-// const user = require('../models/user');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
@@ -8,14 +7,18 @@ module.exports.getCards = (req, res) => {
 };
 
 module.exports.createCard = (req, res) => {
-  const ownerId = req.user._id;
+  // const owner = {};
+  // owner._id = req.user._id;
+  // console.log(owner);
   const { name, link } = req.body;
 
-  Card.create({ name, link, owner: ownerId })
+  Card.create({ name, link, owner: { _id: req.user._id } })
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.removeCard = (req, res) => {
-
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
