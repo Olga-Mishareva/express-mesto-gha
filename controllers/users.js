4ba3ae3909f093bb7a8b0bad23b75bb947cc5051
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-06-14 15:36:59 +0200

    feat: add update user data & avatar

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,25 @@ module.exports.getUser = (req, res) => {
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
+
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.updateUserInfo = (req, res) => {
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    // upsert: true, - если не найден, будет создан новый пользователь
+    .then((newUser) => res.send({ data: newUser }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .then((newUser) => res.send({ data: newUser }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
