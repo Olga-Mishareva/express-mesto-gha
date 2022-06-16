424059df4e343b184c6ddfe0662f5c7bd49cfd8f
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-06-16 20:55:42 +0200

    fix: error by not found route

--- app.js
+++ app.js
@@ -23,4 +23,8 @@ app.use((req, res, next) => {
 app.use('/users', users);
 app.use('/cards', cards);
 
+app.use((req, res) => {
+  res.status(404).send({ message: 'Путь не найден' });
+});
+
 app.listen(PORT);
