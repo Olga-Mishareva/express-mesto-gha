63785a35dea202c7731e6ead26b235d39cda3bfc
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-06-24 15:20:36 +0200

    feat: edit user schema

--- models/user.js
+++ models/user.js
@@ -3,19 +3,29 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    default: 'Missis M',
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
+    default: 'Traveler',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
+    default: 'https://images.unsplash.com/photo-1464820453369-31d2c0b651af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+  },
+  email: {
+    type: String,
     required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 });
 
