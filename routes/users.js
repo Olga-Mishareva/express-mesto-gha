5cff95dad2273a157595e8ed7d8ed0badbe372bd
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-06-30 19:19:27 +0200

    fix: update user validation

--- routes/users.js
+++ routes/users.js
@@ -7,16 +7,28 @@ const {
   updateUserInfo,
   updateAvatar,
 } = require('../controllers/users');
+const { linkRegex } = require('../utils/constants');
 
 router.get('/', getUsers);
 router.get('/me', getMe);
+
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().alphanum().length(24),
   }),
 }), getUser);
 
-router.patch('/me', updateUserInfo);
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUserInfo);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(linkRegex),
+  }),
+}), updateAvatar);
 
 module.exports = router;
