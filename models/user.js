0ebe996ee3e18eda5656f1c327a526508c7a00d4
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-06-30 13:49:36 +0200

    feat: add validation by schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { emailRegex, linkRegex } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -15,12 +16,18 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
+    validate: {
+      validator: (value) => linkRegex.test(value),
+    },
     default: 'https://images.unsplash.com/photo-1464820453369-31d2c0b651af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    validate: {
+      validator: (value) => emailRegex.test(value),
+    },
   },
   password: {
     type: String,
